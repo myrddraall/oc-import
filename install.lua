3a0ef146bf3e4339109937f68691b8e44f06f5a4
Erd <erd@conceptual.ca>
2018-11-25 13:17:30 -0500

    installdir support

--- install.lua
+++ install.lua
@@ -1,6 +1,14 @@
 local shell = require("shell");
-shell.execute('rm /usr/lib/import.lua');
-shell.execute('cp -r ./lib/* /usr/lib/');
 
-shell.execute('rm /boot/04_import.lua');
-shell.execute('cp -r ./boot/* /boot/');
+local args = {...};
+local installDir = "";
+if args[1] then
+    installDir = args[1];
+end
+
+
+shell.execute('rm ' .. installDir .. '/usr/lib/import.lua');
+shell.execute('cp -r ./lib/* ' .. installDir .. '/usr/lib/');
+
+shell.execute('rm ' .. installDir .. '/boot/04_import.lua');
+shell.execute('cp -r ./boot/* ' .. installDir .. '/boot/');
